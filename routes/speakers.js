2c19de3dbcebfaaac78c4072be7d26a1d9090f36
zokaiG <futlouco7@gmail.com>
2020-08-11 11:05:30 -0400

    Added data/ and services/ and made use of them in the '/speakers' and '/feedback' routes.

--- routes/speakers.js
+++ routes/speakers.js
@@ -2,9 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-module.exports = () => {
-  router.get('/', (req, res) => {
-    res.send('Speakers list');
+module.exports = (params) => {
+  const { speakersService } = params;
+
+  router.get('/', async (req, res) => {
+    const speakers = await speakersService.getList();
+    res.json(speakers);
   });
 
   router.get('/:shortname', (req, res) => {
