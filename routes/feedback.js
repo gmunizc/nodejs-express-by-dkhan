bf50d4ae53967744bcd86ed74b5658d44f374637
zokaiG <futlouco7@gmail.com>
2020-08-11 18:50:28 -0400

    Created an api endpoint for creating a new feedback.

--- routes/feedback.js
+++ routes/feedback.js
@@ -3,6 +3,18 @@ const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
+const validations = [
+  check('name').trim().isLength({ min: 3 }).escape().withMessage('A name is required'),
+  check('email')
+    .trim()
+    .isEmail()
+    .normalizeEmail()
+    .escape()
+    .withMessage('A valid email address is required'),
+  check('title').trim().isLength({ min: 3 }).escape().withMessage('A title is required'),
+  check('message').trim().isLength({ min: 5 }).escape().withMessage('A message is required'),
+];
+
 module.exports = (params) => {
   const { feedbackService } = params;
 
@@ -25,20 +37,8 @@ module.exports = (params) => {
     }
   });
 
-  router.post(
-    '/',
-    [
-      check('name').trim().isLength({ min: 3 }).escape().withMessage('A name is required'),
-      check('email')
-        .trim()
-        .isEmail()
-        .normalizeEmail()
-        .escape()
-        .withMessage('A valid email address is required'),
-      check('title').trim().isLength({ min: 3 }).escape().withMessage('A title is required'),
-      check('message').trim().isLength({ min: 5 }).escape().withMessage('A message is required'),
-    ],
-    async (req, res) => {
+  router.post('/', validations, async (req, res, next) => {
+    try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         req.session.feedback = {
@@ -53,8 +53,26 @@ module.exports = (params) => {
         };
         res.redirect('/feedback');
       }
+    } catch (error) {
+      next(error);
     }
-  );
+  });
+
+  router.post('/api', validations, async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.json({ errors: errors.array() });
+      } else {
+        const { name, email, title, message } = req.body;
+        await feedbackService.addEntry(name, email, title, message);
+        const feedback = await feedbackService.getList();
+        res.json({ feedback });
+      }
+    } catch (error) {
+      next(error);
+    }
+  });
 
   return router;
 };
